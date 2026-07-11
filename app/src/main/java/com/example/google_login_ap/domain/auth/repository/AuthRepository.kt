9b66c7181cffbb5bc894d8b8936fd82617c78a1a
LEXA-14 <114798705+LEXA-14@users.noreply.github.com>
2026-07-10 22:20:35 -0400

    Feat: Agrega Auth repository

---
+++ app/src/main/java/com/example/google_login_ap/domain/auth/repository/AuthRepository.kt
@@ -0,0 +1,19 @@
+package com.example.google_login_ap.domain.auth.repository
+
+import android.content.Context
+import androidx.credentials.CredentialManager
+import androidx.credentials.CustomCredential
+import androidx.credentials.GetCredentialRequest
+import com.google.android.libraries.identity.googleid.GetGoogleIdOption
+import com.google.android.libraries.identity.googleid.GoogleIdTokenCredential
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
+import com.google.firebase.auth.GoogleAuthProvider
+import kotlinx.coroutines.tasks.await
+import javax.inject.Inject
+
+interface AuthRepository {
+    suspend fun signInWithGoogle(context: Context): Result<FirebaseUser>
+    suspend fun signOut()
+    fun getCurrentUser(): FirebaseUser?
+}
